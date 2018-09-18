a0aa40d2f1b27343f7ee1403becd4830bff2fcb0
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-09-18 13:58:04 -0600

    This displays my favorite quote from the movie, "The Lion King."

---
+++ src/AlannaChapter2/Chapter2Program3.java
@@ -0,0 +1,16 @@
+package AlannaChapter2;
+/*
+ * Alanna Botscharow
+ * 9/18
+ * Favorite quote. 
+ */
+public class Chapter2Program3 {
+
+	public static void main(String[] args) {
+		//Displays a favorite quote of mine. 
+		
+		System.out.println(" \"I'm surrounded by idiots\" -Jeremy Irons (Scar)" );
+
+	}
+
+}
